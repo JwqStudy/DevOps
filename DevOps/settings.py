115c75029ea4273151d0c874ef3123277aa9552c
Jiang <923258717@qq.com>
2018-08-02 23:26:30 +0800

    20180802

--- DevOps/settings.py
+++ DevOps/settings.py
@@ -37,13 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'saltstack',
+    'blog',
+    'public'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -76,8 +79,12 @@ WSGI_APPLICATION = 'DevOps.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DevOps',
+        'USER': 'devops',
+        'PASSWORD': 'devops',
+        'HOST': '192.168.137.130',
+        'PORT': '3306',
     }
 }
 
--- DevOps/urls.py
+++ DevOps/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from public import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('saltstack/', include('saltstack.urls')),
+    path('blog/', include('blog.urls')),
+    path('public/', include('public.urls')),
+    path('login/', view=views.login),
+    path('signup/', view=views.signup),
+    url('^$', view=views.opsIndex),
 ]
